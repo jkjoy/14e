c25ef4ffe76be19a781f37a2e02b3f005f722d33
浪子 <jkjoy@live.cn>
2024-02-19 18:47:12 +0800

    Update memos.js

--- js/memos.js
+++ js/memos.js
@@ -89,7 +89,7 @@ fetch(bbUrl).then(res => res.json()).then( resdata =>{
   }
   //在未展开评论时，默认显示评论数
   Artalk.loadCountWidget({
-    server: 'https://artalk.loliko.cn/',
+    server: 'https://art.loliko.cn/',
     site: '14e', 
     countEl: '#ArtalkCount'
   });
@@ -248,7 +248,7 @@ function getTagNow(e){
 
   //在未展开评论时，默认显示评论数
   Artalk.loadCountWidget({
-    server: 'https://artalk.loliko.cn/',
+    server: 'https://art.loliko.cn/',
     site: '14e', 
     countEl: '#ArtalkCount'
   });
@@ -348,7 +348,7 @@ function loadArtalk(memo_id) {
       el: '#memo_' + memo_id,
       pageKey: '/m/' + memo_id,
       pageTitle: '',
-      server: 'https://artalk.loliko.cn/',
+      server: 'https://art.loliko.cn/',
       site: '14e',
       darkMode: 'auto'
     });
